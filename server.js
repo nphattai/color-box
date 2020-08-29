21c08f8fcad9d69b916df3d2b1629ef6c99601ae
Nguyen Tai <phat-tai.nguyen@ant-tech.eu>
2020-08-29 16:06:17 +0700

    format code

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(cors());
+app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')))
 
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
 }
 
-app.listen(PORT, error => {
-    if (error) throw error;
-    console.log("Server running on port ", PORT);
-});
+app.listen(PORT, (error) => {
+  if (error) throw error
+  console.log('Server running on port ', PORT)
+})
